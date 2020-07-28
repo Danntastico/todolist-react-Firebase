0926a7b547020448c6e6cc0842a6bb903d3880e8
Danilo Peña <nicolasdpf@gmail.com>
2020-07-28 14:15:42 -0500

    Modified main tasks styles

--- src/containers/TaskList/Tasks.jsx
+++ src/containers/TaskList/Tasks.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { Task } from '../../components/Task/Task';
+import { FloatingBtn } from '../../components/floatingBtn/FloatingBtn';
 
 export const Tasks = () => {
   return (
-    <div className='tasks'>
+    <div className='tasks container'>
       <div className='tasks__header'>
-        <h1>
-          Tareas <span className='header__remainTasks'>(3 asignadas)</span>
-        </h1>
+        <div className='description'>
+          <h1>Tareas</h1>
+          <p>(3 asignadas)</p>
+        </div>
+      </div>
+      <div className='tasks__aside'>
+        <div className='description'>
+          <h1>Tareas</h1>
+          <p>(3 asignadas)</p>
+        </div>
+      </div>
+      <div className='tasks__list'>
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+        <Task />
         <Task />
       </div>
+      <FloatingBtn />
     </div>
   );
 };
