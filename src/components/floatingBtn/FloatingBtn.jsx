1d845b04692de835b968c2ed4f70f7651fed96f8
Danilo Peña <nicolasdpf@gmail.com>
2020-07-28 14:15:26 -0500

    Added floating button component and styles

---
+++ src/components/floatingBtn/FloatingBtn.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const FloatingBtn = () => {
+  return (
+    <button className='floatingBtn btn'>
+      <i className='fas fa-plus'></i>
+    </button>
+  );
+};
