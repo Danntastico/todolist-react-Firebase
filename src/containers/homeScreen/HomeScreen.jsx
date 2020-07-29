40bb08b90929b865172c4dbd16cf192eb39b3196
Danilo Peña <nicolasdpf@gmail.com>
2020-07-29 12:19:44 -0500

    HOme screen Changes

--- src/containers/homeScreen/HomeScreen.jsx
+++ src/containers/homeScreen/HomeScreen.jsx
@@ -11,12 +11,12 @@ import moment from 'moment';
 import { TaskEntries } from '../../components/taskEntries/TaskEntries';
 
 export const HomeScreen = () => {
+  const [loading, setLoading] = useState(true);
   const { todos } = useSelector((state) => state.tasks);
-  const { name } = useSelector((state) => state.auth);
-
+  const { name, photoURL } = useSelector((state) => state.auth);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [realTime, setRealTime] = useState(moment(), 'YYYY-MM-DD hh:mm:ss');
-
+  console.log(name);
   const addTaskModal = () => {
     setModalIsOpen(true);
   };
@@ -31,36 +31,53 @@ export const HomeScreen = () => {
     }, 1000);
   };
 
-  useEffect(() => {
+  /* useEffect(() => {
     thick();
     return () => {
       clearInterval(thick);
     };
-  }, [realTime.getSeconds]);
+  }, [realTime.getSeconds]); */
+
+  useEffect(() => {
+    if (!todos) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [todos]);
 
   return (
     <>
       <AsideNav handleClick={addTaskModal} />
-      <div className='tasks container'>
-        {todos.length === 0 ? (
-          <h1> Aun no tiene tareas </h1>
-        ) : (
-          <>
-            <TaskListHeader />
-            <TaskEntries>
-              {todos.map((todo) => (
-                <SingleTask key={todo.id} author={name} {...todo} />
-              ))}
-            </TaskEntries>
-          </>
-        )}
-        <AddTaskModal
-          modalIsOpen={modalIsOpen}
-          closeModal={handleCloseModal}
-          currentTime={realTime.format('yyyy-MM-DDThh:mm')}
-        />
-        <FloatingBtn handleClick={addTaskModal} />
-      </div>
+      {loading ? (
+        <h1>Loading!</h1>
+      ) : (
+        <div className='tasks container'>
+          {todos.length === 0 ? (
+            <h1> Aun no tiene tareas </h1>
+          ) : (
+            <>
+              <TaskListHeader />
+              <TaskEntries>
+                {todos.map((todo) => (
+                  <SingleTask
+                    key={todo.id}
+                    author={name}
+                    photoURL={photoURL}
+                    {...todo}
+                  />
+                ))}
+              </TaskEntries>
+            </>
+          )}
+          <AddTaskModal
+            modalIsOpen={modalIsOpen}
+            closeModal={handleCloseModal}
+            currentTime={realTime.format('yyyy-MM-DDThh:mm')}
+          />
+          <FloatingBtn handleClick={addTaskModal} />
+        </div>
+      )}
     </>
   );
 };
