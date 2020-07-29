c5b6d7f1ad370e186967cf5ebc60fefb73ce99d1
Danilo Peña <nicolasdpf@gmail.com>
2020-07-28 19:08:30 -0500

    Added dispatch to floatingBtn

--- src/components/floatingBtn/FloatingBtn.jsx
+++ src/components/floatingBtn/FloatingBtn.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { startNewNote } from '../../store/actions/todos';
 
 export const FloatingBtn = () => {
+  const dispatch = useDispatch();
+  const handleAddNew = () => {
+    dispatch(startNewNote());
+  };
   return (
-    <button className='floatingBtn btn'>
+    <button className='floatingBtn btn' onClick={handleAddNew}>
       <i className='fas fa-plus'></i>
     </button>
   );
